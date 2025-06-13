5b1e8613306a991914e7726084486469083bd74a
bartek2050 <bartlomiej.ciastek@gmail.com>
2025-06-13 09:49:36 +0200

    add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import "./header.css";
+
+export const Header = () => {
+  return (
+    <div className="header-wrapper">
+      <img src="/crypto-grid.png" alt="Crypto Grid" />
+      <h2>Crypto Grid</h2>
+    </div>
+  );
+};
