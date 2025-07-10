e3d8b80ccdf45780a75fa56a65ec656556144085
bartek2050 <bartlomiej.ciastek@gmail.com>
2025-07-10 12:41:05 +0200

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import "./css/Footer.css";
+
+export const Footer = () => {
+  return (
+    <div className="footer-wrapper">
+      <a href="https://github.com/bartek2050" target="_blank">
+        <img src="src/assets/github-mark-white.png" alt="Github Mark White" />
+        <span>bartek2050</span>
+      </a>
+    </div>
+  );
+};
--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -2,6 +2,7 @@ import { GlobalData } from "../components/GlobalData.tsx";
 import { TopCoins } from "../components/TopCoins.tsx";
 import { Treemap } from "../components/Treemap.tsx";
 import { Header } from "../components/Header.tsx";
+import { Footer } from "../components/Footer.tsx";
 
 export const MainView = () => {
 
@@ -11,6 +12,7 @@ export const MainView = () => {
       <GlobalData />
       <Treemap />
       <TopCoins />
+      <Footer />
     </>
   );
 };
